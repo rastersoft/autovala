bc877856703d0b7635ddbf3ae551a4a72e3deb8f
Sergio Costas <raster@rastersoft.com>
2017-02-11 19:12:42 +0100

    Updated documentation

--- wiki/convert.sh
+++ wiki/convert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp ../HISTORY.md ./versions.md
 for file in *.md; do
     cat header.html > "../doc/`basename $file .md`"
     pandoc  -f markdown_github -t html "$file" >> "../doc/`basename $file .md`"
